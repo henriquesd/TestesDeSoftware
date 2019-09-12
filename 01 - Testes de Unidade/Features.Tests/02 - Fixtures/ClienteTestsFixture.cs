e122a43f16fc3cbb46bd424d885f1dda202c6cf0
henriquesd <hsd.foz@gmail.com>
2019-09-12 19:40:41 -0300

    Utilizando Fixtures

---
+++ 01 - Testes de Unidade/Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
@@ -0,0 +1,48 @@
+﻿using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests
+{
+    // [CollectionDefinition("ClienteCollection")]
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture>
+    {}
+
+    // Por essa classe ser reaproveitável, entre as outras classes de testes que irão utilizar, ela implementa o IDisposable;
+    // O Objeto é criado e ele fica disponível para todos os testes que estiverem rodando dentro da coleção;
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Henrique",
+                "Domareski",
+                DateTime.Now.AddYears(-30),
+                "henrique@henrique.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "henrique2henrique.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
