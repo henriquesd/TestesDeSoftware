a6a36e610e08254c3757937ce1b77f9b60ccfe60
henriquesd <hsd.foz@gmail.com>
2019-09-16 17:41:38 -0300

    Simulando retorno de métodos com Mock

--- 01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
+++ 01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
@@ -36,5 +36,47 @@ namespace Features.Tests
             clienteRepo.Verify(r => r.Adicionar(cliente), Times.Once);
             mediatr.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once);
         }
+
+        [Fact(DisplayName = "Adicionar Cliente com Falha")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+            // Arrange
+            var cliente = _clienteTestsBogus.GerarClienteInvalido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
+
+            var clienteService = new ClienteService(clienteRepo.Object, mediatr.Object);
+
+            // Act
+            clienteService.Adicionar(cliente);
+
+            // Assert
+            Assert.False(cliente.EhValido());
+            clienteRepo.Verify(r => r.Adicionar(cliente), Times.Never);
+            mediatr.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never);
+        }
+
+        [Fact(DisplayName = "Obter Clientes Ativos")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
+        {
+            // Arrange
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
+
+            clienteRepo.Setup(c => c.ObterTodos())
+                .Returns(_clienteTestsBogus.ObterClientesVariados());
+
+            var clienteService = new ClienteService(clienteRepo.Object, mediatr.Object);
+
+            // Act
+            var clientes = clienteService.ObterTodosAtivos();
+
+            // Assert 
+            clienteRepo.Verify(r => r.ObterTodos(), Times.Once);
+            Assert.True(clientes.Any());
+            Assert.False(clientes.Count(c => !c.Ativo) > 0);
+        }
     }
 }
