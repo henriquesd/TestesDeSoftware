dcb6572a0955291f686e1012fe1f089acb6436d5
henriquesd <hsd.foz@gmail.com>
2019-10-02 16:41:25 -0300

    Pedido - Remover Item

--- 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -84,6 +84,15 @@ namespace NerdStore.Vendas.Domain
             CalcularValorPedido();
         }
 
+        public void RemoverItem(PedidoItem pedidoItem)
+        {
+            ValidarPedidoItemInexistente(pedidoItem);
+
+            _pedidoItems.Remove(pedidoItem);
+
+            CalcularValorPedido();
+        }
+
         public void TornarRascunho()
         {
             PedidoStatus = PedidoStatus.Rascunho;
